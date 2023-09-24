64262fc404c5f26a23b99f404bd07fa5fadc105b
nadiaS11 <nadiasultana.ns11@gmail.com>
2023-09-24 18:51:44 +0600

    adjusted the AppliedJobs component when there's no job applied to

--- src/Route/MainLayout/AppliedJob.jsx
+++ src/Route/MainLayout/AppliedJob.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
 const AppliedJob = ({ job }) => {
   const {
@@ -14,16 +15,31 @@ const AppliedJob = ({ job }) => {
 
   return (
     <div>
-      <div className="card rounded py-2 px-2 md:flex-row justify-center items-center bg-base-100  border border-solid border-gray-500">
+      <div className="flex flex-col rounded py-2 px-2 md:flex-row justify-center items-center bg-base-100  border border-solid border-gray-500">
         <figure className="w-2/5">
           <img src={logo} alt="Movie" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">New movie is released!</h2>
+          <h2 className="card-title">{job_title}</h2>
           <p>{company_name}</p>
+          <div className="flex gap-2 mb-3">
+            <p className="border-gradient px-2 py-1  font-medium">
+              {remote_or_onsite}
+            </p>
+            <p className="border-gradient  px-2 py-1 font-medium">{job_type}</p>
+          </div>
+          <p className="mb-3 font-semibold text-xl text-[#757575] mx-auto">
+            <small className="mr-4">{location}</small>
+            <small>Salary: {salary} </small>
+          </p>
         </div>
         <div className="card-actions px-2">
-          <button className="btn btn-primary">View Details</button>
+          <NavLink
+            to={`/job-details/${id}`}
+            className=" rounded btn-gradient text-center py-1 px-2  "
+          >
+            View Details
+          </NavLink>
         </div>
       </div>
     </div>
--- src/Route/MainLayout/AppliedJobs.jsx
+++ src/Route/MainLayout/AppliedJobs.jsx
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import AppliedJob from "./AppliedJob";
 
-const AppliedJobs = (props) => {
+const AppliedJobs = () => {
   const appliedJobs = JSON.parse(localStorage.getItem("applied"));
-  console.log(appliedJobs);
+  const [displayJobs, setDisplayJobs] = useState(appliedJobs || []);
+  console.log(displayJobs);
+
+  const handleJobFilter = (filter) => {
+    if (filter === "all") {
+      setDisplayJobs(appliedJobs);
+    } else if (filter === "onsite") {
+      const onsiteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Onsite"
+      );
+      setDisplayJobs(onsiteJobs);
+    } else if (filter === "remote") {
+      const remoteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Remote"
+      );
+      setDisplayJobs(remoteJobs);
+    }
+  };
+
   return (
     <div>
-      applied job here
-      <div className="py-5 grid gap-5 w-2/3 mx-auto">
-        {appliedJobs.map((job) => (
+      {displayJobs.length > 0 && (
+        <div className="text-right pr-4  pt-4">
+          <details className="dropdown  rounded mb-32">
+            <summary className="m-1 btn">Filter</summary>
+            <ul className="p-2 shadow menu dropdown-content z-[1]   rounded-box w-52">
+              <li onClick={() => handleJobFilter("all")}>
+                <a>All</a>
+              </li>
+              <li onClick={() => handleJobFilter("onsite")}>
+                <a>Onsite</a>
+              </li>
+              <li onClick={() => handleJobFilter("remote")}>
+                <a>Remote</a>
+              </li>
+            </ul>
+          </details>
+        </div>
+      )}
+
+      <div className="py-4 grid gap-5 w-2/3 mx-auto">
+        {/* {displayJobs.map((job) => (
           <AppliedJob key={job.id} job={job}></AppliedJob>
-        ))}
+        ))} */}
+        {displayJobs.length === 0 ? (
+          <p className="text-center font-bold text-2xl pt-52">
+            No jobs applied.
+          </p>
+        ) : (
+          displayJobs.map((job) => (
+            <AppliedJob key={job.id} job={job}></AppliedJob>
+          ))
+        )}
       </div>
     </div>
   );
